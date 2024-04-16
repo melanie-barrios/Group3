69f808d11b51f8fe0c5cd5b5f9114e22708091cb
avocadocoder73 <113757969+avocadocoder73@users.noreply.github.com>
2024-04-15 21:22:17 -0500

    adjusted models.py

--- TA APP/models.py
+++ TA APP/models.py
@@ -1,7 +1,29 @@
-class User:
+from django.db import models
+
+
+
+class Instructor(models.Model):
+    user_id = models.ForeignKey('Users', on_delete=models.CASCADE)
+    instructor_id =  models.IntegerField()
+
+class TA(models.Model):
+    user_id = models.ForeignKey('Users', on_delete=models.CASCADE)
+    ta_id = models.IntegerField(primary_key=True)
+
+
+
+
+class User(models.Model):
     """
     Represents a user in the system.
     """
+
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    password = models.CharField()
+    name = models.CharField()
+    phone_number = models.CharField(max_length=11)
+    address = models.TextField()
+    email = models.TextField()
 
     def get_user_info(self) -> dict:
         """
@@ -34,10 +56,16 @@ class User:
         """
 
 
-class Course:
+class Course(models.Model):
     """
     Represents a course in the system.
     """
+
+    course_id = models.CharField(primary_key=True)
+    course_name = models.CharField(max_length=120)
+    course_code = models.IntegerField()
+    instructor_id = models.ForeignKey('Instructor', on_delete=models.CASCADE)
+    lab_id = models.IntegerField()
 
     def get_course_info(self) -> dict:
         """
@@ -70,10 +98,15 @@ class Course:
         """
 
 
-class LabSection:
+class LabSection(models.Model):
     """
     Represents a lab section in the system.
     """
+
+    course_id = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    course_code = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    lab_id = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    ta_id = models.ForeignKey(TA, on_delete=models.CASCADE)
 
     def get_lab_section_info(self) -> dict:
         """
