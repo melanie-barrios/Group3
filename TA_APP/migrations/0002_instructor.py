c1a690ed3add0647cf974e0c7cbe018c2c55aa2d
Mohammad Alsheikh <alsheik2@uwm.edu>
2024-04-19 15:58:00 -0500

    Changed folder name

--- TA_APP/migrations/0002_instructor.py
+++ TA_APP/migrations/0002_instructor.py
@@ -3,13 +3,13 @@ from django.db import migrations, models
 import django.db.models.deletion
 class Migration(migrations.Migration):
 
-    dependencies = ['TA APP','0001_Users.py']
+    dependencies = ['TA_APP','0001_Users.py']
 
     operations = [
         migrations.CreateModel(
             name='Instructor',
             fields=[
-                ('user_id',models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='TA APP.Users')),
+                ('user_id',models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='TA_APP.Users')),
                 ('instructor_id',models.IntegerField(primary_key=True)),
             ],
         ),
--- TA_APP/migrations/0003_ta.py
+++ TA_APP/migrations/0003_ta.py
@@ -2,13 +2,13 @@ from django.db import migrations, models
 import django.db.models.deletion
 class Migration(migrations.Migration):
 
-    dependencies = ['TA APP','0001_Users.py']
+    dependencies = ['TA_APP','0001_Users.py']
 
     operations = [
         migrations.CreateModel(
             name='TA',
             fields=[
-                ('user_id',models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='TA APP.Users')),
+                ('user_id',models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='TA_APP.Users')),
                 ('ta_id',models.IntegerField(primary_key=True)),
             ],
         ),
