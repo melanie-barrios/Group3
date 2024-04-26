6cddd622c317fb12e3f757b5debdc78626759087
avocadocoder73 <113757969+avocadocoder73@users.noreply.github.com>
2024-04-25 21:24:31 -0500

    Update admin.py
    removed unused models

--- TA_APP/admin.py
+++ TA_APP/admin.py
@@ -1,16 +1,13 @@
-from TA_APP.models import Instructor
-from TA_APP.models import TA
-from TA_APP.models import Supervisor
+
 from TA_APP.models import User
 from TA_APP.models import Course
 from TA_APP.models import LabSection
 from django.contrib import admin
 
 
-admin.site.register(Instructor)
-admin.site.register(TA)
+
+
 admin.site.register(User)
-admin.site.register(Supervisor)
 admin.site.register(Course)
 admin.site.register(LabSection)
 
