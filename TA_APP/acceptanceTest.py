8e522008064731917a3bb6b41fbc3f60d798e8de
avocadocoder73 <113757969+avocadocoder73@users.noreply.github.com>
2024-04-27 19:41:41 -0500

    Added acceptance tests for course creation and supervisor account deletion

--- TA_APP/acceptanceTest.py
+++ TA_APP/acceptanceTest.py
@@ -2,6 +2,8 @@ import unittest
 from TA_Scheduling.wsgi import *
 from django.test import Client
 from TA_APP.models import User
+from TA_APP.models import Course
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class LoginTest(unittest.TestCase):
@@ -45,13 +47,46 @@ class SupervisorCreateAccountTest(unittest.TestCase):
 
 
 class SupervisorDeleteAccountTest(unittest.TestCase):
-    def __init__(self):
+    def setUp(self):
+        self.user = User(username="newestuser", password="newestuser2")
+        self.user.save()
         self.client = Client()
+
+    def test_deleteaccount(self):
+
+        response = self.client.post('/homepage/', {"username": "newestuser", "password": "newestuser2", "status": "delete"})
+
+
+        with self.assertRaises(ObjectDoesNotExist):
+            User.objects.get(username="newestuser")
+
+
+
+
+    def test_invaliddeletion(self):
+
+        response = self.client.post('/homepage/', {"username": "newestuser10", "password": "42", "status": "delete"})
+
+        self.assertEqual(response.content, "User does not exist to be deleted")
 
 
 class SupervisorCreateCourseTest(unittest.TestCase):
-    def __init__(self):
+    def setUp(self):
         self.client = Client()
+
+    def createCourse(self):
+        response = self.client.post('/homepage/', {"course_name": "Chemistry 101", "course_term": "F", "status": "create_course"})
+
+        course = Course.objects.get(course_name="Chemistry 101")
+
+        self.assertEqual(course.course_term, "F")
+
+        course.delete()
+
+    def invalidCoursecreation(self):
+        response = self.client.post('/homepage/', {"course_name":"", "status":"create_course"})
+
+        self.assertEqual(response.content, "Missing fields for course creation")
 
 
 class SupervisorDeleteCourseTest(unittest.TestCase):
