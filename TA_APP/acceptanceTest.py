e7b9e8ee4703875004bbe20d6bc9f4069d8e562b
avocadocoder73 <113757969+avocadocoder73@users.noreply.github.com>
2024-05-02 11:54:46 -0500

    finished edit course acceptance tests for supervisor added some pbis for each test as well

--- TA_APP/acceptanceTest.py
+++ TA_APP/acceptanceTest.py
@@ -6,6 +6,12 @@ from TA_APP.models import Course
 from django.core.exceptions import ObjectDoesNotExist
 
 
+
+
+
+"""
+PBI: As a user I would like to be able to sign into my account so that I can access it securely
+"""
 class LoginTest(unittest.TestCase):
     def setUp(self):
         self.user = User(username="newestuser", password="newestuser2")
@@ -39,13 +45,19 @@ class LoginTest(unittest.TestCase):
 
 
 
-
+"""
+PBI: As a supervisor I would like to create accounts for staff so that I can assign their responsibilities
+"""
 
 class SupervisorCreateAccountTest(unittest.TestCase):
     def __init__(self):
         self.client = Client()
 
 
+
+"""
+PBI: As a supervisor I would like to delete accounts for staff so that I can delete accounts of individuals who do not work at the school anymore
+"""
 class SupervisorDeleteAccountTest(unittest.TestCase):
     def setUp(self):
         self.user = User(username="newestuser", password="newestuser2")
@@ -69,6 +81,11 @@ class SupervisorDeleteAccountTest(unittest.TestCase):
 
         self.assertEqual(response.content, "User does not exist to be deleted")
 
+
+
+"""
+PBI: As a supervisor I would like to create courses for staff so that I can designate them to their respective class to teach
+"""
 
 class SupervisorCreateCourseTest(unittest.TestCase):
     def setUp(self):
@@ -107,6 +124,30 @@ class SupervisorDeleteCourseTest(unittest.TestCase):
 
         self.assertEqual(response.content, "User does not exist to be deleted")
 
+
+class SupervisorEditCourseTest(unittest.TestCase):
+    def setUp(self):
+        self.Course = Course(course_name="Chemistry 101")
+        self.Course.save()
+        self.client = Client()
+
+    def validCourseEdit(self):
+        response = self.client.post('/homepage/', {"course_name": "Chemistry 101", "editdata":{"course_name": "Chemistry 102"}, "status": "edit_course"})
+
+        with self.assertRaises(ObjectDoesNotExist):
+            Course.objects.get(course_name="Chemistry 101")
+
+    def invalidCourseEdit(self):
+
+        response = self.client.post('/homepage/', {"course_name": "Chemistry 102", "editdata": {"course_name": "Chemistry 103"}, "status": "edit_course"})
+
+        self.assertEqual(response.content, "Course does not exist to be edited")
+
+
+
+"""
+PBI: As a supervisor I would like to assign users to courses so that the proper faculty and registered students are enrolled
+"""
 class SupervisorAssignUserToCourseTest(unittest.TestCase):
     def __init__(self):
         self.client = Client()
